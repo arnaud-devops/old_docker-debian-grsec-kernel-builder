1d98b8c3d7baaed17914fe8df548cccca4b4386a
Arnaud Astruc <oxynux@gmail.com>
2017-02-12 22:17:02 +0100

    not remove Docker image

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ docker run -itd --name debian-grsec-kernel-builder debian-grsec-kernel-builder
 docker cp debian-grsec-kernel-builder:/root/linux-kernel .
 docker kill debian-grsec-kernel-builder
 docker rm debian-grsec-kernel-builder
-docker rmi debian-grsec-kernel-builder
